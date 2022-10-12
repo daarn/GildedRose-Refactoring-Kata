b8f97ccff6e1982ef5f4c00723a77987d5652fa2
daarn <darrenmcd37@hotmail.co.uk>
2022-10-12 17:38:41 +0100

    Push GildedRose refactor for Civica tech task

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,4 +1,9 @@
 class GildedRose
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+
+  MAX_QUALITY = 50
 
   def initialize(items)
     @items = items
@@ -6,51 +11,65 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      next if item.name == SULFURAS
+      
+      item.sell_in -= 1
+      
+      quality_decrement = conjured?(item) ? 2 : 1
+
+      case item.name
+      when BRIE
+        update_brie(item)
+      when BACKSTAGE_PASS
+        update_backstage_pass(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_standard_item(item, quality_decrement)
       end
     end
   end
+
+  def update_brie(item)
+    return if item.quality == MAX_QUALITY
+
+    item.quality += 1
+  end
+
+  def update_backstage_pass(item)
+    return item.quality = 0 if past_sell_date?(item)
+    return if item.quality == MAX_QUALITY
+
+    if item.sell_in <= 5
+      item.quality += 3
+    elsif item.sell_in <= 10
+      item.quality += 2
+    else
+      item.quality += 1
+    end
+
+    max_quality?(item)
+  end
+
+  def update_standard_item(item, quality_decrement)
+    return if item.quality == 0
+    
+    item.quality -= quality_decrement
+    item.quality -= quality_decrement if past_sell_date?(item)
+
+    item.quality = 0 if item.quality.negative?
+  end
+
+  def conjured?(item)
+    item.name.start_with?("Conjured")
+  end
+
+  def max_quality?(item)
+    item.quality = MAX_QUALITY if item.quality >= MAX_QUALITY
+  end
+
+  def past_sell_date?(item)
+    item.sell_in.negative?
+  end
+
 end
 
 class Item
--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -3,10 +3,126 @@ require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
 
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
+  FOO_ITEM = [Item.new("foo", 0, 0)]
+  SULFURAS = [Item.new("Sulfuras, Hand of Ragnaros", 10, 80)]
+  BACKSTAGE_PASSES = [ 
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20)
+  ]
+  CONJURED = [Item.new("Conjured Sword", 10, 10)]
+
+  def test_can_create_valid_item
+    items = FOO_ITEM
+    assert_equal items[0].name, "foo"
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 0
+  end
+
+  def test_can_update_quality
+    items = FOO_ITEM
+    assert GildedRose.new(items).update_quality()
+  end
+
+  def test_can_to_s
+    items = FOO_ITEM
+    assert_equal "foo, 0, 0", items[0].to_s
+  end
+
+  def test_normal_item_update
+    items = [Item.new("Sword", 10, 20)]
+    shop = GildedRose.new(items)
+
+    for i in 1..10 do
+      shop.update_quality()
+      assert_equal items[0].sell_in, 10-i
+      assert_equal items[0].quality, 20-i
+    end
+  end
+
+  def test_brie_update
+    items = [Item.new("Aged Brie", 10, 10)]
+    shop = GildedRose.new(items)
+
+    for i in 1..10 do
+      shop.update_quality()
+      assert_equal items[0].sell_in, 10-i
+      assert_equal items[0].quality, 10+i
+    end
+  end
+
+  def test_sulfuras_update
+    items = SULFURAS
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+
+    assert_equal items[0].sell_in, 10
+    assert_equal items[0].quality, 80
+  end
+
+  def test_backstage_passes_update
+    items = BACKSTAGE_PASSES
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 14
+    assert_equal items[1].sell_in, 9
+    assert_equal items[2].sell_in, 4
+
+    assert_equal items[0].quality, 21
+    assert_equal items[1].quality, 22
+    assert_equal items[2].quality, 23
+  end
+
+  def test_conjured_update
+    items = CONJURED
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 9
+    assert_equal items[0].quality, 8
+  end
+
+  def test_quality_min
+    items = FOO_ITEM
+    GildedRose.new(items).update_quality()
+    assert items[0].quality == 0
+  end
+
+  def test_brie_quality_max
+    items = [Item.new("Aged Brie", 10, 50)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 50
+
+    items = [Item.new("Aged Brie", 0, 50)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 50
+  end
+
+  def test_backstage_passes_quality_max
+    items = [ 
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=49),
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49)
+    ]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].quality, 50
+    assert_equal items[1].quality, 50
+    assert_equal items[2].quality, 50
+  end
+
+  def test_backstage_passes_after_concert
+    items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=49)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 0
+  end
+
+  def test_quality_past_sell
+    items = [Item.new("Sword", 0, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 18
+
+    items = [Item.new("Conjured Sword", 0, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 16
   end
 
 end
